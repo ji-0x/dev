ae88c2c7ecd8493d76cfbfdabebc2b520ed63d0f
ji-0x <jamininia.dev@gmail.com>
2025-09-10 20:52:13 +1200

    created function to fetch coingecko data, and defined coins list

--- projects/financial_pipeline/scripts/extract_coingecko.py
+++ projects/financial_pipeline/scripts/extract_coingecko.py
@@ -27,3 +27,46 @@ os.makedirs(LOG_DIR,exist_ok=True)
 logger = setup_logger(__name__, LOG_DIR, 'extract_coingecko')
 logger.info('Logger initialised for extract_coingecko.py')
 
+
+# -----------------------------
+# Fetch coingecko market data
+# -----------------------------
+
+def fetch_coingecko_data(coin_ids, vs_currency='usd'):
+    url = "https://api.coingecko.com/api/v3/coins/markets"
+    params = {
+        "vs_currency": vs_currency,
+        "ids": ','.join(coin_ids),
+        "order": "market_cap_desc",
+        "per_page": len(coin_ids),
+        "page": 1,
+        "sparkline": "false",
+        "price_change_percentage": "1h,24h,7d"
+    }
+
+    try:
+        response = requests.get(url, params=params, timeout=10)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        logger.error(f"Failed to fetch coingecko data: {e}")
+        return None
+
+
+
+def main():
+    # Define coins list
+    coins = [
+        'bitcoin', 'ethereum', 'solana', 'ripple', 'cardano',
+        'dogecoin','stellar','hedera-hashgraph','algorand','vechain',
+        'bnb'
+    ]
+
+    try:
+
+
+    except Exception as e:
+    
+
+if __name__ == '__main__':
+    main()
