2da93875e1dd4439b609c58dc21b55495d292b9d
ji-0x <jamininia.dev@gmail.com>
2025-09-12 19:02:25 +1200

    fet(build): added API parameters
    API parameters defined in a dictionary in script temporarily

--- projects/financial_pipeline/scripts/extract_forex.py
+++ projects/financial_pipeline/scripts/extract_forex.py
@@ -6,8 +6,6 @@ import requests
 import time 
 from datetime import datetime
 from financial_pipeline.utils.logging_utils import setup_logger
-from projects.financial_pipeline.scripts.extract_coingecko import LOG_DIR, RAW_DATA_DIR
-
 
 
 
@@ -15,7 +13,14 @@ from projects.financial_pipeline.scripts.extract_coingecko import LOG_DIR, RAW_D
 # Setup
 # -----------------------------
 
-CONFIG = {}
+CONFIG = {
+    "api_key": "",
+    "fx_function": "FX_DAILY",
+    "base_currency": "USD",
+    "target_currencies": ["EUR", "GBP", "JPY", "AUD", "NZD", "CAD"],
+    "api_url": "https://www.alphavantage.co/query",
+    "rate_limit_delay": 12
+}
 
 # -----------------------------
 # paths
@@ -25,15 +30,14 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
 LOG_DIR = os.path.join(BASE_DIR, 'logs')
 RAW_DATA_DIR = os.path.join(BASE_DIR, 'data', 'raw')
 
-
 # -----------------------------
 # Logging
 # -----------------------------
-
-logger. = setup_logger(__name__, LOG_DIR, 'extract_forex')
+logger  = setup_logger(__name__, LOG_DIR, 'extract_forex')
 logger.info("Logger initialised for extract_forex.py")
 
+# -----------------------------
+# Fetch forex rates from Aplha vantage
+# -----------------------------
 
-# -----------------------------
-# g
-# -----------------------------
+def 
